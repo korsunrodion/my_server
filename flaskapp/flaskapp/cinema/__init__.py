ac4450d5127553da1d0bc8a2212d53cdf929b5cc
Rodion Korsun <korsun.rodion@gmail.com>
2021-03-09 03:53:50 +0200

    cleanup

--- flaskapp/flaskapp/cinema/__init__.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, Blueprint, render_template
-
-cinema_app = Blueprint('cinema_app', __name__, template_folder='react')
-
-@cinema_app.route('/')
-def index():
-    return render_template('cinema.html')
-
-
